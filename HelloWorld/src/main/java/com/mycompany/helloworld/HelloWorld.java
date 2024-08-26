d648592612686a884976ea9055352c4ddcc0ec4c
rpaternina <robertpaternina65@gmail.com>
2024-08-26 16:45:28 -0500

    Mi primer hola mundo en java

---
+++ HelloWorld/src/main/java/com/mycompany/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.helloworld;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
