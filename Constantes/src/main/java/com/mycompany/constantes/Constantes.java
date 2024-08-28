0b6f0099b0b152bc4b7a3adfcd49757d8e5cf705
rpaternina <robertpaternina65@gmail.com>
2024-08-28 12:35:15 -0500

    constantes en java

---
+++ Constantes/src/main/java/com/mycompany/constantes/Constantes.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.constantes;
+
+/**
+ *
+ * @author 57300
+ */
+public class Constantes {
+
+    public static void main(String[] args) {
+        final int number = 1;
+        System.out.println(number);
+        
+    }
+}
