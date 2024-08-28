6a2b8fc1db792136ebe63fae68a9e01c0135b661
rpaternina <robertpaternina65@gmail.com>
2024-08-28 12:42:12 -0500

    Operadores aritmeticos

---
+++ OperadoresAritmeticos/src/main/java/com/mycompany/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.operadoresaritmeticos;
+
+/**
+ *
+ * @author 57300
+ */
+public class OperadoresAritmeticos {
+
+    public static void main(String[] args) {
+        int number1 = 10, number2 = 20;
+        int resultado;
+        resultado = number1 + number2;
+        
+        System.out.println(resultado);
+    }
+}
