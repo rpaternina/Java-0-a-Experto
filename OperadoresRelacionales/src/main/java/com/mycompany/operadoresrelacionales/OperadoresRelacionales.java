311cc77f42bf39123530fe2a906c06af13c8849f
rpaternina <robertpaternina65@gmail.com>
2024-08-29 12:41:23 -0500

    Operadores relacionales

---
+++ OperadoresRelacionales/src/main/java/com/mycompany/operadoresrelacionales/OperadoresRelacionales.java
@@ -0,0 +1,16 @@
+package com.mycompany.operadoresrelacionales;
+
+public class OperadoresRelacionales {
+
+    public static void main(String[] args) {
+        int valor = 500, valor2 = 300;
+        boolean resultado;
+        
+        resultado = valor < valor2;
+        System.out.println(resultado);
+        
+        resultado = valor > valor2;
+        System.out.println(resultado);
+        
+    }
+}
