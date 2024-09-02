8567a7caa64167fe5960adc05cbc3fb0838f4704
rpaternina <robertpaternina65@gmail.com>
2024-09-02 15:45:33 -0500

    Strings y sus metodos

--- Stringd1/src/main/java/com/mycompany/stringd1/Stringd2.java
+++ Stringd1/src/main/java/com/mycompany/stringd1/Stringd2.java
@@ -5,13 +5,15 @@ public class Stringd2 {
     public static void main(String[] args) {
         //Comparando String
         
-        String mensaje1 = " Hola";
-        String mensaje2 = "Hola    ";
+        String mensaje1 = "Hola ", mensaje2 = "hOla";
         
         //equals() nos sirve para hacer la comparación entre dos strings
         boolean resultado = mensaje1.trim().equals(mensaje2.trim());
-        
         System.out.println(resultado);
+        
+        //equalsIgnoreCase() sirve para comparar el texto sin importar que esten diferentes siempre y cuando sean el mismo texto
+        boolean resultado1 = mensaje1.equalsIgnoreCase(mensaje2);
+        System.out.println(resultado1);
     }
     
 }
