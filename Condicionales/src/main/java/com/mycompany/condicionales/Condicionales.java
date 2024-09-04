2f33325453453d1c879a82aa067266092d76c783
rpaternina <robertpaternina65@gmail.com>
2024-09-04 14:56:33 -0500

    Condicionales en java

---
+++ Condicionales/src/main/java/com/mycompany/condicionales/Condicionales.java
@@ -0,0 +1,14 @@
+package com.mycompany.condicionales;
+
+public class Condicionales {
+
+    public static void main(String[] args) {
+                String color = "Verde";
+        
+        if(color.equalsIgnoreCase("verde")){
+            System.out.println("Puede pasar");
+        } else {
+            System.out.println("Alto");
+        }
+    }
+    }
