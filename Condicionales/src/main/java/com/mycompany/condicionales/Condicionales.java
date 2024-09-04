d3144aeea5a9a27ae2bc650469a70178298912a9
rpaternina <robertpaternina65@gmail.com>
2024-09-04 15:01:39 -0500

    Condicionales

--- Condicionales/src/main/java/com/mycompany/condicionales/Condicionales.java
+++ Condicionales/src/main/java/com/mycompany/condicionales/Condicionales.java
@@ -3,10 +3,12 @@ package com.mycompany.condicionales;
 public class Condicionales {
 
     public static void main(String[] args) {
-                String color = "Verde";
+                String color = "amarillo";
         
         if(color.equalsIgnoreCase("verde")){
             System.out.println("Puede pasar");
+        } else if (color.equalsIgnoreCase("Amarillo")){
+            System.out.println("Preparese para arrancar");
         } else {
             System.out.println("Alto");
         }
