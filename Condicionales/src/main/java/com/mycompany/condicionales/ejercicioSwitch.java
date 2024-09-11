2c7c784c7dad599ff299de3adc01c5c2f51454ed
rpaternina <robertpaternina65@gmail.com>
2024-09-11 15:14:03 -0500

    Practicando condicionales

--- Condicionales/src/main/java/com/mycompany/condicionales/ejercicioSwitch.java
+++ Condicionales/src/main/java/com/mycompany/condicionales/ejercicioSwitch.java
@@ -30,6 +30,22 @@ public class ejercicioSwitch {
                 resta = rnumero1 - rnumero2;
                 JOptionPane.showMessageDialog(null,"El resultado de la resta es: " + resta);
             break;
+            
+            case 3:
+                float dividir, dnumero1, dnumero2;
+                dnumero1 = Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese un numero: "));
+                dnumero2 = Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese un numero: "));
+                dividir = dnumero1 / dnumero2;
+                JOptionPane.showMessageDialog(null,"El resultado de la división es: " + dividir);
+                break;
+            
+            case 4:
+                int multi, mnumero1, mnumero2;
+                mnumero1 = Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese un numero: "));
+                mnumero2 = Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese un numero: "));
+                multi = mnumero1 * mnumero2;
+                JOptionPane.showMessageDialog(null,"El resultado de la multiplicación es es: " + multi);
+               
         }
     }
     
