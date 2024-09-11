6493c2c11e5f6953b20b5048d76a7fdfc413ce13
rpaternina <robertpaternina65@gmail.com>
2024-09-11 16:42:50 -0500

    Practicando con ciclos y condicionales

--- Condicionales/src/main/java/com/mycompany/condicionales/bucleFor.java
+++ Condicionales/src/main/java/com/mycompany/condicionales/bucleFor.java
@@ -1,11 +1,23 @@
 package com.mycompany.condicionales;
 
+import javax.swing.JOptionPane;
+
 public class bucleFor {
 
     public static void main(String[] args) {
-        for(int i = 0; i < 100; i+=10){
-            System.out.println(i);
+        //Tabla de multiplicar
+        int numero = Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese un numero"));
+        if(numero <= 0){
+            JOptionPane.showMessageDialog(null, "Ingrese un numero mayor a 0");
+        }
+        
+        int contador;
+        for(contador = 1; contador <= numero; contador++){
+            
+            System.out.println(numero + " * " + contador + " = " + numero*contador);
+        }
+        
         }
     }
     
-}
+
