9b2e252795bf2df3f2ead533b46bd3a6662be705
rpaternina <robertpaternina65@gmail.com>
2024-09-18 13:29:07 -0500

    Arreglos

--- Arreglos/src/main/java/com/mycompany/arreglos/Arreglos.java
+++ Arreglos/src/main/java/com/mycompany/arreglos/Arreglos.java
@@ -5,11 +5,7 @@ import javax.swing.JOptionPane;
 public class Arreglos {
 
     public static void main(String[] args) {
-        String nombres [] = new String[3];
-        
-        nombres[0] = "Robert";
-        nombres[1] = "Keiry";
-        nombres[2] = "Samuel";
+        String nombres [] = {"Robert","Keiry","Samuel"};
         
         JOptionPane.showMessageDialog(null, nombres[0]+ ", " + nombres[1]+", " + nombres[2]);
     }
