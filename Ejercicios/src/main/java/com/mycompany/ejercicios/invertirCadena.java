4ae052330eced2f817434d01c3ad85b7ff615897
rpaternina <robertpaternina65@gmail.com>
2024-09-19 13:54:18 -0500

    Practicando, Arrays, bucles, condicinales

--- Ejercicios/src/main/java/com/mycompany/ejercicios/invertirCadena.java
+++ Ejercicios/src/main/java/com/mycompany/ejercicios/invertirCadena.java
@@ -1,12 +1,28 @@
 
+//Escribe un programa que tome una cadena de texto dada por el usuario y la invierta. Por ejemplo, "hola" se convertiría en "aloh
 package com.mycompany.ejercicios;
 
+import javax.swing.JOptionPane;
 
 public class invertirCadena {
 
   
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        String cadena = JOptionPane.showInputDialog(null, "Ingrese la cadena a invertir");
+        
+        char[] caracteres = new char[cadena.length()];
+        
+        String invertida = "";
+        
+        for(int i = cadena.length() - 1; i >= 0; i--){
+            caracteres[i] = cadena.charAt(i);
+            invertida += caracteres[i];
+           
+        }
+        
+        JOptionPane.showMessageDialog(null, "La cadena invertida es: " + invertida);
+       
     }
     
 }
