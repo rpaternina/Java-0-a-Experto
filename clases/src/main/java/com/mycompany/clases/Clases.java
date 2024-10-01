7a7f9de8c46f48a27bb778d399eaf63fa3846125
rpaternina <robertpaternina65@gmail.com>
2024-10-01 13:10:08 -0500

    entrando en poo

---
+++ clases/src/main/java/com/mycompany/clases/Clases.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clases;
+
+/**
+ *
+ * @author 57300
+ */
+public class Clases {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ clases/src/main/java/com/mycompany/clases/Perro.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.clases;
+
+/**
+ *
+ * @author 57300
+ */
+public class Perro {
+    
+}
