de4f0d2589c167e7abf0ed68bc5c67455bb9b2d2
rpaternina <robertpaternina65@gmail.com>
2024-10-02 17:42:48 -0500

    poo

---
+++ GestionEstudiantes/src/main/java/com/mycompany/gestionestudiantes/Estudiante.java
@@ -0,0 +1,36 @@
+
+package com.mycompany.gestionestudiantes;
+
+public class Estudiante {
+
+    String nombre;
+    int edad;
+    String notas;
+
+    
+    //constructor vacio
+    public Estudiante() {
+    }
+    
+    
+    //los metodos son acciones que pueda hacer la clase
+    public void registrarAlumno(){
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
