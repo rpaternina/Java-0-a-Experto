6254250bee86450d07375fa67afe3e1be90e35d1
rpaternina <robertpaternina65@gmail.com>
2024-10-02 16:48:19 -0500

    practicando poo

---
+++ GestionEstudiantes/src/main/java/com/mycompany/gestionestudiantes/GestionEstudiantes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestionestudiantes;
+
+/**
+ *
+ * @author 57300
+ */
+public class GestionEstudiantes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
