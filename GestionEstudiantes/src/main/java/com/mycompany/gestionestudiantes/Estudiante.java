b5a31be8ed0419bade2d17c25b26eb83a4247714
rpaternina <robertpaternina65@gmail.com>
2024-10-03 16:47:34 -0500

    Gestion de estudiantes con poo

--- GestionEstudiantes/src/main/java/com/mycompany/gestionestudiantes/Estudiante.java
+++ GestionEstudiantes/src/main/java/com/mycompany/gestionestudiantes/Estudiante.java
@@ -1,3 +1,10 @@
+/*
+     Crea un programa que permita ingresar datos de estudiantes (nombre, edad, notas), 
+    realizar operaciones como:
+    - Mostrar todos los estudiantes.
+    - Buscar estudiantes por nombre.
+    - Calcular el promedio de notas.
+ */
 
 package com.mycompany.gestionestudiantes;
 
@@ -5,32 +12,51 @@ public class Estudiante {
 
     String nombre;
     int edad;
-    String notas;
+    float notas;
 
-    
-    //constructor vacio
     public Estudiante() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public float getNotas() {
+        return notas;
+    }
+
+    public void setNotas(float notas) {
+        this.notas = notas;
     }
     
     
-    //los metodos son acciones que pueda hacer la clase
-    public void registrarAlumno(){
     
-}
+    public void ingresarDatos(){
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        String nombres [] = new String [1]; 
+        int edades [] = new int[1];
+        float notas [] = new float[3];
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
--- GestionEstudiantes/src/main/java/com/mycompany/gestionestudiantes/GestionEstudiantes.java
+++ GestionEstudiantes/src/main/java/com/mycompany/gestionestudiantes/GestionEstudiantes.java
@@ -1,16 +1,18 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+    Crea un programa que permita ingresar datos de estudiantes (nombre, edad, notas), 
+    realizar operaciones como:
+    - Mostrar todos los estudiantes.
+    - Buscar estudiantes por nombre.
+    - Calcular el promedio de notas.
  */
 
 package com.mycompany.gestionestudiantes;
 
-/**
- *
- * @author 57300
- */
 public class GestionEstudiantes {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        
+        
     }
 }
