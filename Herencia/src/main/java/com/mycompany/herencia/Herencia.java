55570763b46b91b513b386c79adcf47f60aded33
rpaternina <robertpaternina65@gmail.com>
2024-10-09 17:17:34 -0500

    Herencia

---
+++ Herencia/src/main/java/com/mycompany/herencia/Herencia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.herencia;
+
+/**
+ *
+ * @author 57300
+ */
+public class Herencia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
